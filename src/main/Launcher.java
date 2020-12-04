dcd75cb84c6b08f2314d5a383d3143fefc5f0acb
Soup321 <XxkittycatxX321@gmail.com>
2020-12-03 21:26:02 -0500

    Forgot to save file >:(((

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -3,10 +3,6 @@ package main;
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-<<<<<<< HEAD
-        System.out.println("Hello, World! is overrated");
-=======
 //        System.out.println("Hello, World! is overrated"); there shall only be one hello world here :/
->>>>>>> e5001117af51a904621a1c76d24b0a2bbfce79b4
     }
 }
